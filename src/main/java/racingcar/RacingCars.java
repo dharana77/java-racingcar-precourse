6a7476a1e5f8eb86ebf9aaf1c028994cf3e63415
dharana <dharana7723@gmail.com>
2022-04-25 21:36:02 +0900

    feat: 레이싱 카 리스트 처리하는 racingCars 추가

---
+++ src/main/java/racingcar/RacingCars.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RacingCars {
+    private List<String> racingCars = new LinkedList<>();
+
+    public RacingCars(){
+    }
+
+    public void addCar(String racingCar){
+        racingCars.add(racingCar);
+    }
+
+    public List<String> returnRacingCars(){
+        return racingCars;
+    }
+}
